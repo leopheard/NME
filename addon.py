356fe6d48178fc35cc6a32fc6b2ddca82cfcc8d7
leopheard <leopheard.misc@gmail.com>
2020-09-11 21:13:43 -0400

    upload

--- addon.py
+++ addon.py
@@ -1,37 +1,19 @@
 from xbmcswift2 import Plugin, xbmcgui
-from resources.lib import mainaddon
-
 plugin = Plugin()
-url1 = "https://listen-nme.sharp-stream.com/nme1.mp3"
-url2 = "https://listen-nme.sharp-stream.com/nme2.mp3"
 @plugin.route('/')
 def main_menu():
     items = [
         {
             'label': plugin.get_string(30001), 
-            'path': plugin.url_for('episodes1'),
+            'path': "https://listen-nme.sharp-stream.com/nme1.mp3",
             'thumbnail': "https://radio.nme.com/static/media/nme-1.b6b0cc81.png",
             'is_playable': True},
         {
             'label': plugin.get_string(30002),
-            'path': plugin.url_for('episodes2'),
+            'path': "https://listen-nme.sharp-stream.com/nme2.mp3",
             'thumbnail': "https://radio.nme.com/static/media/nme-2.a3f6a801.png",
             'is_playable': True},
     ]
-    return items
-
-@plugin.route('/episodes1/')
-def episodes1():
-    soup1 = mainaddon.get_soup1(url1)
-    playable_podcast1 = mainaddon.get_playable_podcast1(soup1)
-    items = mainaddon.compile_playable_podcast1(playable_podcast1)
-    return items
-
-@plugin.route('/episodes2/')
-def episodes2():
-    soup2 = mainaddon.get_soup2(url2)
-    playable_podcast2 = mainaddon.get_playable_podcast2(soup2)
-    items = mainaddon.compile_playable_podcast2(playable_podcast2)
     return items
 
 if __name__ == '__main__':
